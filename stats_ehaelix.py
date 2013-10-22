fc01f6d1232e5b0c660f6f769f325e7667a01c3a
Guillaume Abrioux <guillaume.abrioux@enovance.com>
2013-10-22 11:07:31 +0200

    TODO: parse the output to retrieve only the vzlist in the vzlist -a cmd

--- stats_ehaelix.py
+++ stats_ehaelix.py
@@ -4,10 +4,16 @@ import os
 import subprocess
 import sys
 
+host = "eno-eh9-b2.mut-8.hosting.enovance.com"
+
 def exec_command_by_ssh(host, command):
 	process = subprocess.Popen("ssh %s '%s'" % (host, command), stdout=subprocess.PIPE, stderr=None, shell=True)                                                                                                                                                          
 	output = process.communicate()
 	return output[0].split()
 
-test = exec_command_by_ssh("remy-web1.ext.hosting.enovance.com", "ls /home")
+def get_vz_list(host):
+	vzlist = exec_command_by_ssh(host, "vzlist -a")
+	return vzlist
+
+test = get_vz_list(host)
 print test
