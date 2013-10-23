7a46d1a144d7e2dec97f886711d92ab2e69800db
Guillaume Abrioux <guillaume.abrioux@enovance.com>
2013-10-23 10:26:57 +0200

    save

--- stats_ehaelix.py
+++ stats_ehaelix.py
@@ -11,6 +11,12 @@ def exec_command_by_ssh(host, command):
 	output = process.communicate()
 	return output[0].split("\n")
 
+def exec_command_on_vz(host, vz_id, command):
+	print "ssh %s \"vzctl exec %s \\\"%s\\\"\"" % (host, vz_id, command)
+	process = subprocess.Popen("ssh %s \"vzctl exec %s \\\"%s\\\"\"" % (host, vz_id, command), stdout=subprocess.PIPE, stderr=None, shell=True)
+	output = process.communicate()
+	return output[0].split("\n")
+
 def get_vz_list(host):
 	vzlist_raw = exec_command_by_ssh(host, "vzlist -a -H -o ctid,hostname,status")
 	vzlist = []
@@ -21,8 +27,18 @@ def get_vz_list(host):
 			'id': vzinfo[0],
 			'hostname': vzinfo[1],
 			'status': vzinfo[2],
+			'physical_host': host
 			})
 	return vzlist
 
-test = get_vz_list(host)
-print test
+def get_cpu_info_vz(vzlist):
+	for vz in vzlist:
+		command = "cat /proc/cpuinfo | grep 'cpu MHz'"
+		tmp = exec_command_on_vz(vz['physical_host'], vz['id'], command)
+		print vz['hostname']
+		print tmp
+		print ""
+		
+
+vzlist = get_vz_list(host)
+get_cpu_info_vz(vzlist)
