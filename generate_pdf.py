ce2ed8260fce769d788ffed6e49342cb04d9e785
gaelL <gael.lambert@enovance.com>
2013-11-25 14:58:36 +0100

    test quick vz template

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,16 +1,28 @@
 from jinja2 import Environment, FileSystemLoader
+import argparse
 import os
 
 from stats_ehaelix import Ehaelix
 
-
-SRV = Ehaelix("eno-eh9-b2.mut-8.hosting.enovance.com")
-print SRV.get_cpu_info()
-
-exit(1)
-
+# Config
 TEMPLATE_DIR='./templates'
 DOCS_DIR='./docs'
+
+# Get args
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-1", "--b1",
+            help="Address of the host (B1)", type=str, required=True)
+PARSER.add_argument("-2", "--b2",
+            help="Address of the second host (B2)", type=str)
+ARGS = PARSER.parse_args()
+
+
+
+
+# Tests :
+SRV = Ehaelix(ARGS.b1)
+VZ = SRV.get_vz_list().pop()
+
 
 
 env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
@@ -18,17 +30,19 @@ env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 filename = 'vz/101.rst'
 template = env.get_template(filename)
 
-class Link(object):
-    href = None
-    caption = None
-    def __init__(self):
-        pass
+#class Link(object):
+#    href = None
+#    caption = None
+#    def __init__(self):
+#        pass
+#
+#link = Link()
+#link.href = 'Link'
+#link.caption = 'Link name'
+#navigation = [link]
+#context = {'a_variable': 'Un truc', 'navigation': navigation}
 
-link = Link()
-link.href = 'Link'
-link.caption = 'Link name'
-navigation = [link]
-context = {'a_variable': 'Un truc', 'navigation': navigation}
+context = {'VZ': VZ}
 
 print '----- %s in %s' % (os.path.basename(filename) ,os.path.dirname(filename))
 print template.render(context)
