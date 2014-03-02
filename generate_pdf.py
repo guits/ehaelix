834772c15b98b9032d75418f00b1e6ce1c8667ce
Guits <guillaume.abrioux@enovance.com>
2014-03-02 18:11:55 +0100

    Add render for physical machine

--- generate_pdf.py
+++ generate_pdf.py
@@ -1,3 +1,6 @@
+'''
+Generate pdf
+'''
 #!/usr/bin/env python
 
 from jinja2 import Environment, FileSystemLoader
@@ -8,8 +11,8 @@ import os
 from stats_ehaelix import Ehaelix
 
 # Config
-TEMPLATE_DIR='./templates'
-DOCS_DIR='./docs'
+TEMPLATE_DIR = './templates'
+DOCS_DIR = './docs'
 
 # Get args
 PARSER = argparse.ArgumentParser()
@@ -19,32 +22,45 @@ PARSER.add_argument("-2", "--b2",
             help="Address of the second host (B2)", type=str)
 ARGS = PARSER.parse_args()
 
-env = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
+ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
 def render(template, destfile=None, context={}, dry_run=False):
+    """
+    render function
+    """
     if destfile:
-        filename=destfile
+        filename = destfile
     else:
-        filename=template
-    template = env.get_template(filename)
+        filename = template
+    template = ENV.get_template(filename)
     try:
         if dry_run:
             print template.render(context)
         else:
             # Make needed directory
-            print '----- %s in %s' % (os.path.basename(filename) ,os.path.dirname(filename))
+            print '----- %s in %s' % (os.path.basename(filename),
+                                      os.path.dirname(filename))
             # Generate File
             template.stream(context).dump( '%s/%s' % (DOCS_DIR, filename))
-    except UndefinedError as e:
-        print 'Generate template %s error var : %s' % (filename, e)
+    except UndefinedError as error:
+        print 'Generate template %s error var : %s' % (filename, error)
 
 # Render all VZ
 SRV = Ehaelix(ARGS.b1)
-VZs = SRV.get_vz_list()
-for vz in VZs:
+VZS = SRV.get_vz_list()
+PHYSICAL = []
+for vz in VZS:
     vz['ram'] = SRV.get_total_mem_info_vz(vz['id'])
     vz['cpu'] = SRV.get_cpu_info_vz(vz['id'])
     vz['disks'] = SRV.get_df_vz_infos(vz['id'])
     vz['apps'] = SRV.get_vz_list_apps(vz['id'])
     render('vz/vz.rst', context={'VZ':vz}, dry_run=True)
 
+# Render Physical host
+PHYSICAL['cpu'] = SRV.get_cpu_info()
+PHYSICAL['vgs'] = SRV.get_vgs_infos()
+PHYSICAL['lvs'] = SRV.get_lvs_infos()
+PHYSICAL['mount'] = SRV.get_mount_infos()
+PHYSICAL['disks'] = SRV.get_df_infos()
+
+render('physical/physical.rst', context={'PHYSICAL':PHYSICAL}, dry_run=True)
